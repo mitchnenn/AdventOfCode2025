c1f6bae1c85c751df3dfbe433045dda95ce3fb79
Mitch Nenn <mitch.nenn@earthlink.net>
2025-12-01 21:22:34 -0800

    Can parse ranges.

--- Day_02/Program.cs
+++ Day_02/Program.cs
@@ -5,23 +5,27 @@ using Common;
 var path = "sample_input.txt";
 //var path = "input.txt";
 
-var lines = path.ReadAllLines();
-lines.ToList().ForEach(Console.WriteLine);
+var ranges = path.ReadAllText()
+    .Split(new[] {',', '\n'}, StringSplitOptions.RemoveEmptyEntries)
+    .Select(l => l.Split('-'))
+    .Select(parts => (int.Parse(parts[0]),int.Parse(parts[1])))
+    .ToList();
+ranges.ForEach((l) => Console.WriteLine($"{l.Item1}-{l.Item2}"));
 
 // Part 1
-var result1 = SolvePart1(lines);
+var result1 = SolvePart1(ranges);
 Console.WriteLine($"Part 1: {result1}");
 
 // Part 2
-var result2 = SolvePart2(lines);
+var result2 = SolvePart2(ranges);
 Console.WriteLine($"Part 2: {result2}");
 
-static int SolvePart1(string[] input) 
+static int SolvePart1(IEnumerable<(int,int)> input) 
 { 
     return 0; 
 }
 
-static int SolvePart2(string[] input) 
+static int SolvePart2(IEnumerable<(int,int)> input) 
 { 
     return 0; 
 }
