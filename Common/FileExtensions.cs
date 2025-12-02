c88bf3782ea4a288ec5b8a686da1c5693ae19d94
Mitch Nenn <mitch.nenn@earthlink.net>
2025-12-01 20:50:43 -0800

    Add input files and trim lines being read from a file.

--- Common/FileExtensions.cs
+++ Common/FileExtensions.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Linq;
 
 namespace Common;
 
@@ -6,11 +7,14 @@ public static class FileExtensions
 {
    public static string[] ReadAllLines(this string path)
    {
-      return File.ReadAllLines(path);
+      return File.ReadAllLines(path)
+         .Select(l => l.Trim())
+         .ToArray();
    }
 
    public static string ReadAllText(this string path)
    {
-      return File.ReadAllText(path).Trim();
+      return File.ReadAllText(path)
+         .Trim();
    }
 }
