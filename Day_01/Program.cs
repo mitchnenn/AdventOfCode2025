eadf2c341745c4b70927728f35535c7c993d510e
Mitch Nenn <mitch.nenn@earthlink.net>
2025-12-01 20:18:00 -0800

    Day 1.

--- Day_01/Program.cs
+++ Day_01/Program.cs
@@ -1,5 +1,83 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System;
+using System.Text.RegularExpressions;
+using Common;
 
-Console.WriteLine("Hello, World!");
+//var path = "sample_input.txt";
+var path = "input.txt";
+
+var lines = path.ReadAllLines();
+lines.ToList().ForEach(Console.WriteLine);
+
+// Part 1
+var result1 = SolvePart1(lines);
+Console.WriteLine($"Part 1: {result1}");
+
+// Part 2
+var result2 = SolvePart2(lines);
+Console.WriteLine($"Part 2: {result2}");
+
+static int SolvePart1(string[] input)
+{
+    int result = 0;
+    const int maxStep = 99;
+    int currentStep = 50;
+    foreach (var combo in input)
+    {
+        int steps = ParseComboTurn(combo);
+        currentStep = Turn(currentStep, maxStep, steps);
+        if(currentStep == 0) result++;
+    }
+    return result;
+}
+
+static int Turn(int currentStep, int maxSteps, int steps)
+{
+    var newStep = currentStep;
+    for (var i = 0; i < Math.Abs(steps); i++)
+    {
+        newStep = steps < 0 ? newStep - 1 : newStep + 1;
+        if (newStep < 0) newStep = maxSteps;
+        else if (newStep > maxSteps) newStep = 0;
+    }
+    return newStep;
+}
+
+static int ParseComboTurn(string combo)
+{
+    var regex = new Regex(@"^([LR])(\d+)$", RegexOptions.Compiled);
+    var match = regex.Match(combo.ToUpper());
+    if (!match.Success) throw new Exception();
+    var turn = match.Groups[1].Value[0];
+    var steps = int.Parse(match.Groups[2].Value);
+    return turn == 'R' ? steps : -steps;
+}
+
+static int SolvePart2(string[] input)
+{
+    int result = 0;
+    const int maxStep = 99;
+    int currentStep = 50;
+    foreach (var combo in input)
+    {
+        int steps = ParseComboTurn(combo);
+        (currentStep, var zeroCout) = TurnAndCountZero(currentStep, maxStep, steps);
+        result += zeroCout;
+    }
+    return result;
+}
+
+
+static (int, int) TurnAndCountZero(int currentStep, int maxSteps, int steps)
+{
+    var newStep = currentStep;
+    var zeroCount = 0;
+    for (var i = 0; i < Math.Abs(steps); i++)
+    {
+        newStep = steps < 0 ? newStep - 1 : newStep + 1;
+        if (newStep < 0) newStep = maxSteps;
+        else if (newStep > maxSteps) newStep = 0;
+        if (newStep == 0) zeroCount++;
+    }
+    return (newStep, zeroCount);
+}
