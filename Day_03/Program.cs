ad5136efebdc490b55596221ae745f072119dc3b
Mitch Nenn <mitch.nenn@earthlink.net>
2025-12-20 10:01:54 -0800

    Day 3 part 2 sample input ... Thanks HyperNeutrino!

--- Day_03/Program.cs
+++ Day_03/Program.cs
@@ -2,8 +2,8 @@
 
 using Common;
 
-//var path = "sample_input.txt";
-var path = "input.txt";
+var path = "sample_input.txt";
+//var path = "input.txt";
 
 var banks = path.ReadAllLines();
 banks.ToList().ForEach(Console.WriteLine);
@@ -19,32 +19,38 @@ Console.WriteLine($"Part 2: {result2}");
 static int SolvePart1(string[] input)
 {
     var sum = 0;
-    var len = 1;
+    var len = 2;
     foreach (var item in input)
     {
         var digits = item.Select(c => c - '0').ToArray();
-        var firstMax = digits[..^len].Max();
+        var firstMax = digits[..^(len-1)].Max();
         var secondMax= digits[(Array.IndexOf(digits, firstMax) + 1)..].Max();
-        sum += firstMax * 10 + secondMax;
+        var max = firstMax * 10 + secondMax;
+        Console.WriteLine(max);
+        sum += max;
     }
     return sum;
 }
 
 static ulong SolvePart2(string[] input)
 {
-    const int len = 12;
     var sum = 0UL;
+    const int len = 12;
     foreach (var item in input)
     {
-        sum += SolveForMax(item, len);
+        var jolts = 0UL;
+        var bank = item.Select(c => c - '0').Select(i => (ulong)i).ToArray();
+        for (var i = 0; i < len - 1; i++)
+        {
+            var digit = bank[..^(len - 1 - i)].Max();
+            bank = bank[(Array.IndexOf(bank, digit) + 1)..];
+            jolts  = jolts * 10 + digit; 
+        }
+        jolts = jolts * 10 + bank.Max();
+        Console.WriteLine(jolts);
+        sum += jolts;
     }
     return sum;
 }
 
-static ulong SolveForMax(string bank, int len)
-{
-    var digits = bank.Select(c => c - '0').ToArray();
-    
-    return 0UL;
-}
 
