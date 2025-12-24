bd35bcb4e5aa7aedc16668ce8a3da09f482c00fa
Mitch Nenn <mitch.nenn@earthlink.net>
2025-12-24 06:40:29 -0800

    Day 5 part 1 solved.

--- Day_05/Program.cs
+++ Day_05/Program.cs
@@ -2,8 +2,8 @@
 
 using Common;
 
-var path = "sample_input.txt";
-//var path = "input.txt";
+//var path = "sample_input.txt";
+var path = "input.txt";
 
 var lines = path.ReadAllLines();
 lines.ToList().ForEach(Console.WriteLine);
@@ -16,12 +16,39 @@ Console.WriteLine($"Part 1: {result1}");
 var result2 = SolvePart2(lines);
 Console.WriteLine($"Part 2: {result2}");
 
-static int SolvePart1(string[] input) 
-{ 
-    return 0; 
+static ulong SolvePart1(string[] input) 
+{
+    ulong count = 0;
+    foreach (var ingredient in GetIngredientIds(input))
+    {
+        if (GetFreshRanges(input).Any(fresh => 
+                ingredient >= fresh.Minimum 
+                && ingredient <= fresh.Maximum))
+        {
+            count++;
+        }
+    }
+    return count;
 }
 
 static int SolvePart2(string[] input) 
 { 
     return 0; 
 }
+
+static IEnumerable<(ulong Minimum, ulong Maximum)> GetFreshRanges(string[] input)
+{
+    return input
+        .TakeWhile(x => !string.IsNullOrEmpty(x))
+        .Select(i => i.Split('-'))
+        .Select(s => (ulong.Parse(s[0]), ulong.Parse(s[1])));
+}
+
+static IEnumerable<ulong> GetIngredientIds(string[] input)
+{
+    return input
+        .SkipWhile(x => x.Contains('-') || string.IsNullOrWhiteSpace(x))
+        .Select(ulong.Parse)
+        .ToList();
+}
+
