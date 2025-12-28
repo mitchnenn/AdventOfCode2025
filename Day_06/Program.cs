3cfe911f3053ba509524720816eabff478024068
Mitch Nenn <mitch.nenn@earthlink.net>
2025-12-28 07:38:28 -0800

    Day 6 part 1 solved.

--- Day_06/Program.cs
+++ Day_06/Program.cs
@@ -1,9 +1,10 @@
 ﻿// Boilerplate structure.
 
+using System.Diagnostics;
 using Common;
 
-var path = "sample_input.txt";
-//var path = "input.txt";
+//var path = "sample_input.txt";
+var path = "input.txt";
 
 var lines = path.ReadAllLines();
 lines.ToList().ForEach(Console.WriteLine);
@@ -16,12 +17,47 @@ Console.WriteLine($"Part 1: {result1}");
 var result2 = SolvePart2(lines);
 Console.WriteLine($"Part 2: {result2}");
 
-static int SolvePart1(string[] input) 
-{ 
-    return 0; 
+static long SolvePart1(string[] input)
+{
+    var count = 0L;
+    foreach (var operaton in ParseInput(input))
+    {
+        var numbers = operaton.Operands.Select(long.Parse);
+        switch (operaton.Operation)
+        {
+            case "+":
+                count += numbers.Aggregate((a, b) => a + b);
+                break;
+            case "*":
+                count += numbers.Aggregate((a, b) => a * b);
+                break;
+            default:
+                Debug.Assert(false, $"Unknown operation: {operaton.Operation}");
+                break;
+        }
+    }
+    return count; 
+}
+
+static IEnumerable<(IEnumerable<string> Operands, string Operation)> ParseInput(string[] input)
+{
+    var rows = input
+        .Select(l => l.Split((char[]?)null, StringSplitOptions.RemoveEmptyEntries))
+        .ToList();
+    var dataRows = rows.SkipLast(1).ToList();
+    var colCount = dataRows[0].Length;
+    var operands = Enumerable.Range(0, colCount)
+        .Select(colIndex => dataRows.Select(r => r[colIndex]).ToList())
+        .ToList();
+    Debug.Assert(operands.Count == rows.Last().Length, "Number of columns doesn't match operations");
+    foreach (var (index, column) in operands.Index())
+    {
+        yield return (column, rows.Last()[index]);
+    }
 }
 
 static int SolvePart2(string[] input) 
 { 
     return 0; 
 }
+
